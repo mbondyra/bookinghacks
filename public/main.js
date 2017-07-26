08c1c2c6c0bb16bcd1167ecd94512eaf56ab03b7
mbondyra <marta.bondyra@gmail.com>
2017-07-26 07:56:31 +0200

    feat: tooltip & better & better

--- public/main.js
+++ public/main.js
@@ -1,7 +1,27 @@
 var mapInstance;
+var markers = [];
+
+var initialData = {
+  "destination": destination,
+  "checkin": checkin,
+  "checkout": checkout,
+  "room1": 'A,A,4',
+  "type": "walking",
+  "time": 3600
+}
+var data = {
+  "destination": destination,
+  "checkin": checkin,
+  "checkout": checkout,
+  "room1": 'A,A,4',
+  "type": "walking",
+  "time": 3600
+}
+
+
 
 var map = {
-  init: function(mainPoint){
+  init: function (mainPoint) {
     mapInstance = new google.maps.Map(document.getElementById('bw-map'), {
       zoom: 14,
       center: {
@@ -13,7 +33,7 @@ var map = {
         position: google.maps.ControlPosition.TOP_CENTER
       }
     });
-    new google.maps.Marker({
+    var destPoint = new google.maps.Marker({
       position: {
         lat: +mainPoint.lat,
         lng: +mainPoint.long
@@ -21,9 +41,26 @@ var map = {
       icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
       map: mapInstance
     });
+
+    var content = "Destination: "+initialData.destination;
+    var infowindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(destPoint, 'click', function() {
+      infowindow.open(mapInstance,destPoint);
+    });
   },
-  createMarker: function(el){
-    return new google.maps.Marker({
+  setMapOnAll: function (dest) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(dest);
+    }
+  },
+  deleteMarkers: function () {
+    map.setMapOnAll(null);
+    markers = [];
+  },
+  addMarker: function (el) {
+    var marker = new google.maps.Marker({
       position: {
         lat: +el.lat,
         lng: +el.long
@@ -31,27 +68,40 @@ var map = {
       icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
       map: mapInstance
     });
+    markers.push(marker);
+
+    var content = "<div>"+el.name+"</div>" +
+      "<div>"+el.price+" "+el.currency_code+"</div>"+
+      "<div><a>more...</a></div>";
+    var infowindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(mapInstance, marker);
+    });
   }
 }
 
+
 var dropdown = {
-  standardTemplate: function(name){
+  standardTemplate: function (name) {
     return '<div>' +
       '<div class="bw-button__opener"></div>' +
       '</div><div class="dropdown dropdown-' + name + ' closed">' +
       '<div class="title">Distance (minutes)</div>' +
       '<div class="dropdown-menu dropdown-menu-' + name + '">' +
       '<ul>' +
-      '<li><5 minutes</li>' +
-      '<li><10 minutes</li>' +
-      '<li><15 minutes</li>' +
-      '<li><30 minutes</li>' +
-      '<li><45 minutes</li>' +
+      '<li data-time="300"><5 minutes</li>' +
+      '<li data-time="600"><10 minutes</li>' +
+      '<li data-time="900"><15 minutes</li>' +
+      '<li data-time="1800"><30 minutes</li>' +
+      '<li data-time="2700"><45 minutes</li>' +
       '</ul>' +
       '</div>' +
       '</div>';
   },
-  init: function(name){
+  init: function (name) {
     var $container = $('.dropdown-menu-' + name),
       $list = $('.dropdown-menu-' + name + ' ul'),
       listItem = $list.find('li');
@@ -65,9 +115,12 @@ var dropdown = {
       }
     });
 
-    $(".dropdown-menu" + name + " li").click(function () {
+    $(".dropdown-" + name + " li").click(function (e) {
       e.stopPropagation();
       closeMenu(this);
+      data.time = $(this).attr('data-time');
+      data.type = name;
+      customGUI.getDataAfterClick()
     });
 
     function closeMenu(el) {
@@ -129,10 +182,10 @@ var preloader = {
       ' </div>'
     document.getElementsByTagName('body')[0].appendChild(preloader);
   },
-  show: function(){
+  show: function () {
     document.getElementById("bw-preloader-layer").className = "visible"
   },
-  hide: function(){
+  hide: function () {
     document.getElementById("bw-preloader-layer").className = ""
   }
 }
@@ -145,38 +198,45 @@ var customGUI = {
     var toolkit = document.createElement('div');
     toolkit.setAttribute('id', 'bw-toolkit');
     document.getElementById('bw-container').appendChild(toolkit);
-    button.init("foot")
-    button.init("car")
-    button.init("bike")
-    button.init("public", "custom")
+    button.init("walking")
+    button.init("driving")
+    button.init("cycling")
+    button.init("public_transport", "custom")
     tooltip.init()
+    customGUI.getInitialData()
   },
-  getData: function(){
+  getInitialData: function () {
+    customGUI.getData(initialData, function (res) {
+      var mainPoint = res.origin;
+      map.init(mainPoint);
+      customGUI.showDataOnMap(res.suitable_hotels)
+      preloader.hide()
+    })
+  },
+  getDataAfterClick: function(){
+    preloader.show();
+    customGUI.getData(data, function (res) {
+      map.deleteMarkers();
+      customGUI.showDataOnMap(res.suitable_hotels)
+      preloader.hide()
+    })
+  },
+  getData: function (data, success) {
     $.ajax({
       type: "POST",
       url: "https://still-hollows-33347.herokuapp.com/api/get_suitable_hotels/",
-      data: {
-        "destination":"Barbican centre",
-        "checkin":"2017-10-22",
-        "checkout":"2017-10-23",
-        "room1":'"A","A",4',
-        "type":"walking",
-      },
-      success: function (res) {
-        var mainPoint =  res.origin;
-        map.init(mainPoint);
-        customGUI.showDataOnMap(res.suitable_hotels)
-      }
+      data: data,
+      success: success
     });
   },
-  showDataOnMap: function(hotels){
+  showDataOnMap: function (hotels) {
     hotels.forEach(function (el) {
-      map.createMarker(el)
+      map.addMarker(el)
     })
-    preloader.hide()
+    map.setMapOnAll(mapInstance)
   }
 }
 
+
 customGUI.init();
-customGUI.getData();
 
